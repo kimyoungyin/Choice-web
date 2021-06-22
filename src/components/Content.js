19c0a325b96da0b58b11882f96ad545f2115f600
kimyoungyin <mafa1234@naver.com>
2021-06-23 02:05:15 +0900

    2021/06/23 #1

--- src/components/Content.js
+++ src/components/Content.js
@@ -12,17 +12,29 @@ const Content = ({ item, userObj }) => {
         history.push(`/detail/${item.id}`);
     };
 
+    const term = (now, when) => {
+        let gap = now - when;
+        let days = Math.floor(gap / (1000 * 60 * 60 * 24));
+        let hours = Math.floor(gap / (1000 * 60 * 60));
+        let minutes = Math.floor((gap / (1000 * 60)) % 60);
+
+        days = days < 10 ? "0" + days : days;
+        hours = hours < 10 ? "0" + hours : hours;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+
+        if (Number(days) != -0) {
+            return `${days}일 ${hours}시간 ${minutes}분 전`;
+        } else if (Number(hours) !== 0) {
+            return `${hours}시간 ${minutes}분 전`;
+        } else {
+            return `${minutes}분 전`;
+        }
+    };
     return (
         <section className="Content" onClick={goToChoiceInfo}>
-            <article className="Content-list">
-                <div>작성자 {item.writer}</div>
-                <div>{item.question}</div>
-            </article>
-            {/* <ChoiceInfo
-                    item={item}
-                    userObj={userObj}
-                    toggleChoiceMode={toggleChoiceMode}
-                /> */}
+            <div>{term(Date.now(), item.when)}</div>
+            <div>작성자 {item.writer}</div>
+            <div>{item.question}</div>
         </section>
     );
 };
