ba9782597c8efd8efecb45e6823f4c640e5db2fd
kimyoungyin <yin199859@gmail.com>
2023-01-29 09:09:48 +0900

    fix: Router 필요없는 클래스명과 route 제거

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { UserObj } from "components/App";
 import Auth from "routes/Auth";
 import Title from "components/Title";
 import Home from "routes/Home";
@@ -9,46 +8,37 @@ import Navigation from "components/Navigation";
 
 interface AppRouterProps {
     isLoggedIn: boolean;
-    userObj: UserObj;
+    userObj: global.User;
 }
 
 const AppRouter = ({ isLoggedIn, userObj }: AppRouterProps) => {
     return (
         <Router>
-            <div className="Router">
-                {isLoggedIn && <Title />}
-                {isLoggedIn ? (
-                    <Switch>
-                        <Route exact path="/">
-                            <Home userObj={userObj} />
-                        </Route>
-                        <Route exact path="/profile">
-                            <Profile userObj={userObj} />
-                        </Route>
-                        <Route
-                            exact
-                            path="/detail/:id" // 자동으로 render props,match.id 타입 생성해줌
-                            render={(props) => (
-                                <ChoiceInfo {...props} userObj={userObj} />
-                            )}
-                        />
-                        {/* <Route
-                            exact
-                            path="/detail"
-                            render={(props) => (
-                                <ChoiceInfo {...props} userObj={userObj} />
-                            )}
-                        /> */}
-                    </Switch>
-                ) : (
-                    <Switch>
-                        <Route exact path="/">
-                            <Auth />
-                        </Route>
-                    </Switch>
-                )}
-                {isLoggedIn && <Navigation />}
-            </div>
+            {isLoggedIn && <Title />}
+            {isLoggedIn ? (
+                <Switch>
+                    <Route exact path="/">
+                        <Home userObj={userObj} />
+                    </Route>
+                    <Route exact path="/profile">
+                        <Profile userObj={userObj} />
+                    </Route>
+                    <Route
+                        exact
+                        path="/detail/:id" // 자동으로 render props,match.id 타입 생성해줌
+                        render={(props) => (
+                            <ChoiceInfo {...props} userObj={userObj} />
+                        )}
+                    />
+                </Switch>
+            ) : (
+                <Switch>
+                    <Route exact path="/">
+                        <Auth />
+                    </Route>
+                </Switch>
+            )}
+            {isLoggedIn && <Navigation />}
         </Router>
     );
 };
