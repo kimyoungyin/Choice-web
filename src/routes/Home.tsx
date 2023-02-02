854cbe2d8f6e6f2261187ff4717d7ea36d264c4b
kimyoungyin <yin199859@gmail.com>
2023-02-03 00:24:09 +0900

    fix: Home 타입에러 수정

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,56 +1,39 @@
 import { ChangeEvent, useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { dbService, storageService } from "../fb";
+// import { v4 as uuidv4 } from "uuid";
 import "../style.css";
 import Content from "../components/Content";
-import AddForm from "../components/AddForm";
+import AddForm, { Category } from "../components/AddForm";
 import Alert from "../components/Alert";
 import customAixos from "../customAixos";
-import { UserObj } from "components/App";
+import { Item } from "routes/ChoiceInfo";
 
-const Home = ({ userObj }: { userObj: UserObj }) => {
+// interface HomeProps {
+//     userObj: global.User;
+// }
+
+// const Home = ({ userObj }: HomeProps) => {
+const Home = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [uploadMode, setUploadMode] = useState(false);
-    const [choiceItems, setChoiceItems] = useState([]);
-    const [categoryValue, setCategoryValue] = useState("current");
+    const [choiceItems, setChoiceItems] = useState<Item[]>([]);
+    // const [categoryValue, setCategoryValue] = useState("current");
     const [categoryInput, setCategoryInput] = useState("");
     const [question, setQuestion] = useState("");
     const [choice1, setChoice1] = useState("");
     const [choice2, setChoice2] = useState("");
     const [attachment1, setAttachment1] = useState("");
     const [attachment2, setAttachment2] = useState("");
-    const [floatingAlert, setFloatingAlert] = useState(false);
-    const [completeUpload, setCompleteUpload] = useState(false);
-    const [filters, setFilters] = useState([]);
-    const [filtering, setFiltering] = useState("");
-    const [value, setValue] = useState("");
-    const activated = document.querySelector(".active");
+    const [isAlertOn, setIsAlertOn] = useState(false);
+    const [isUploadCompleted, setIsUploadCompleted] = useState(false);
+    const [filters, setFilters] = useState<Category[]>([]);
+    const [filterCategoryId, setFilterCategoryId] = useState<number | null>(
+        null
+    );
+    // const [value, setValue] = useState("");
+    // const activated = document.querySelector(".active");
 
     useEffect(() => {
         const asyncFunction = async () => {
-            // await dbService.collection("questions").onSnapshot((snapshot) => {
-            //     const questions = snapshot.docs.map((doc) => ({
-            //         id: doc.id,
-            //         ...doc.data(),
-            //     }));
-            //     questions.sort((a, b) => {
-            //         if (a.when > b.when) return -1;
-            //         if (a.when < b.when) return 1;
-
-            //         return 0;
-            //     });
-            //     setChoiceItems(questions);
-            //     setIsLoading(false);
-            // });
-
-            // await dbService.collection("category").onSnapshot((snapshot) => {
-            //     const categorys = snapshot.docs.map((doc) => ({
-            //         id: doc.id,
-            //         ...doc.data(),
-            //     }));
-            //     categorys.sort();
-            //     setFilters(categorys);
-            // });
             try {
                 const { data: posts } = await customAixos.get("/posts");
                 const { data: categories } = await customAixos.get(
@@ -58,7 +41,6 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
                 );
                 setChoiceItems(posts);
                 setFilters(categories);
-                console.log(posts);
             } catch (error) {
                 console.log(error);
             } finally {
@@ -82,14 +64,14 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
         const {
             target: { files, className },
         } = event;
-        if (!Array.isArray(files)) return;
-        const theFile = files[0];
+        if (!files) return;
+        const theFile = Array.from(files)[0];
         const reader = new FileReader();
         try {
-            reader.onloadend = (finishedEvent) => {
-                const {
-                    currentTarget: { result },
-                } = finishedEvent;
+            reader.onloadend = () => {
+                // if (!currentTarget) return;
+                // const result = currentTarget.result;
+                const result = reader.result as string; // 매우 긴 문자열
                 if (className === "AddForm-choice1Img") {
                     setAttachment1(result);
                 } else if (className === "AddForm-choice2Img") {
@@ -107,118 +89,121 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
     };
 
     const onClearAttachment1 = () => {
-        const file = document.querySelector(".AddForm-choice1Img");
-        file.value = "";
+        // const file = document.querySelector(".AddForm-choice1Img");
+        // file.value = "";
         setAttachment1("");
     };
 
     const onClearAttachment2 = () => {
-        const file = document.querySelector(".AddForm-choice2Img");
-        file.value = "";
+        // const file = document.querySelector(".AddForm-choice2Img");
+        // file.value = "";
         setAttachment2("");
     };
 
-    const categoryRadio = (e) => {
-        const {
-            target: { value },
-        } = e;
-        setCategoryValue(value);
+    const categoryRadio = () => {
+        // const categoryRadio = (e) => {
+        // const {
+        //     target: { value },
+        // } = e;
+        // setCategoryValue(value);
     };
 
-    const onChange = (e) => {
-        const {
-            target: { name, value },
-        } = e;
-        if (name === "newCategory") {
-            setCategoryInput(value);
-        } else if (name === "category") {
-            setCategoryInput(value);
-        } else if (name === "question") {
-            setQuestion(value);
-        } else if (name === "choice1") {
-            setChoice1(value);
-        } else if (name === "choice2") {
-            setChoice2(value);
-        }
+    const onChange = () => {
+        // const onChange = (e) => {
+        // const {
+        //     target: { name, value },
+        // } = e;
+        // if (name === "newCategory") {
+        //     setCategoryInput(value);
+        // } else if (name === "category") {
+        //     setCategoryInput(value);
+        // } else if (name === "question") {
+        //     setQuestion(value);
+        // } else if (name === "choice1") {
+        //     setChoice1(value);
+        // } else if (name === "choice2") {
+        //     setChoice2(value);
+        // }
     };
 
     const startSubmit = async () => {
-        setUploadMode(false);
-        setFloatingAlert(true);
-        let attachment1Url = "";
-        let attachment2Url = "";
-        const attachmentRef1 = storageService
-            .ref()
-            .child(`${userObj.uid}/${uuidv4()}`);
-        const attachmentRef2 = storageService
-            .ref()
-            .child(`${userObj.uid}/${uuidv4()}`);
-        if (attachment1 !== "" && attachment2 !== "") {
-            const response1 = await attachmentRef1.putString(
-                attachment1,
-                "data_url"
-            );
-            attachment1Url = await response1.ref.getDownloadURL();
-            const response2 = await attachmentRef2.putString(
-                attachment2,
-                "data_url"
-            );
-            attachment2Url = await response2.ref.getDownloadURL();
-        } else if (attachment1 !== "") {
-            console.log("1만 빈칸 아님");
-            const response1 = await attachmentRef1.putString(
-                attachment1,
-                "data_url"
-            );
-            attachment1Url = await response1.ref.getDownloadURL();
-        } else if (attachment2 !== "") {
-            console.log("2만 빈칸 아님");
-            const response2 = await attachmentRef2.putString(
-                attachment2,
-                "data_url"
-            );
-            attachment2Url = await response2.ref.getDownloadURL();
-        }
+        //     setUploadMode(false);
+        setIsAlertOn(true);
+        //     let attachment1Url = "";
+        //     let attachment2Url = "";
+        //     const attachmentRef1 = storageService
+        //         .ref()
+        //         .child(`${userObj.uid}/${uuidv4()}`);
+        //     const attachmentRef2 = storageService
+        //         .ref()
+        //         .child(`${userObj.uid}/${uuidv4()}`);
+        //     if (attachment1 !== "" && attachment2 !== "") {
+        //         const response1 = await attachmentRef1.putString(
+        //             attachment1,
+        //             "data_url"
+        //         );
+        //         attachment1Url = await response1.ref.getDownloadURL();
+        //         const response2 = await attachmentRef2.putString(
+        //             attachment2,
+        //             "data_url"
+        //         );
+        //         attachment2Url = await response2.ref.getDownloadURL();
+        //     } else if (attachment1 !== "") {
+        //         console.log("1만 빈칸 아님");
+        //         const response1 = await attachmentRef1.putString(
+        //             attachment1,
+        //             "data_url"
+        //         );
+        //         attachment1Url = await response1.ref.getDownloadURL();
+        //     } else if (attachment2 !== "") {
+        //         console.log("2만 빈칸 아님");
+        //         const response2 = await attachmentRef2.putString(
+        //             attachment2,
+        //             "data_url"
+        //         );
+        //         attachment2Url = await response2.ref.getDownloadURL();
+        //     }
 
-        const contentObj = {
-            category: categoryInput,
-            question: question,
-            choice1: choice1,
-            choice2: choice2,
-            when: Date.now(),
-            writer: userObj.displayName,
-            photo: userObj.photo,
-            attachment1Url,
-            attachment2Url,
-        };
+        //     const contentObj = {
+        //         category: categoryInput,
+        //         question: question,
+        //         choice1: choice1,
+        //         choice2: choice2,
+        //         when: Date.now(),
+        //         writer: userObj.displayName,
+        //         photo: userObj.photo,
+        //         attachment1Url,
+        //         attachment2Url,
+        //     };
 
-        dbService
-            .collection("questions")
-            .add(contentObj)
-            .then(() => setCompleteUpload(true));
-        setCategoryInput("");
-        setQuestion("");
-        setChoice1("");
-        setChoice2("");
-        setAttachment1("");
-        setAttachment2("");
-        // setCompleteUpload(true);
-        if (completeUpload) {
-            setTimeout(() => setFloatingAlert(false), 2000);
+        //     dbService
+        //         .collection("questions")
+        //         .add(contentObj)
+        //         .then(() => setCompleteUpload(true));
+        //     setCategoryInput("");
+        //     setQuestion("");
+        //     setChoice1("");
+        //     setChoice2("");
+        //     setAttachment1("");
+        //     setAttachment2("");
+        setIsUploadCompleted(true);
+        if (isUploadCompleted) {
+            setTimeout(() => setIsAlertOn(false), 2000);
         }
     };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        const filterText = filters.map((filter) => filter.text);
-        if (categoryValue === "new" && filterText.includes(categoryInput)) {
-            alert("이미 카테고리가 있습니다");
-        } else if (categoryValue === "new") {
-            await dbService.collection("category").add({ text: categoryInput });
-            startSubmit();
-        } else if (categoryValue === "current") {
-            startSubmit();
-        }
+    const onSubmit = async () => {
+        // const onSubmit = async (e) => {
+        // e.preventDefault();
+        // const filterText = filters.map((filter) => filter.text);
+        // if (categoryValue === "new" && filterText.includes(categoryInput)) {
+        //     alert("이미 카테고리가 있습니다");
+        // } else if (categoryValue === "new") {
+        //     await dbService.collection("category").add({ text: categoryInput });
+        // startSubmit();
+        // } else if (categoryValue === "current") {
+        startSubmit();
+        // }
     };
 
     const cancelAdd = () => {
@@ -231,26 +216,19 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
         setAttachment2("");
     };
 
-    const onChangeFilter = (e) => {
-        const {
-            target: { value },
-        } = e;
-        setValue(value);
+    const onChangeFilter = () => {
+        // const onChangeFilter = (e) => {
+        // const {
+        //     target: { value },
+        // } = e;
+        // setValue(value);
     };
 
-    const getFilter = (e) => {
-        const {
-            target: { innerText, className },
-        } = e;
-        if (className === "Home-categoryBtn") {
-            if (activated !== null) {
-                activated.classList.remove("active");
-            }
-            setFiltering(innerText);
-            e.target.classList.add("active");
-        } else if (className === "Home-categoryBtn active") {
-            setFiltering("");
-            e.target.classList.remove("active");
+    const getFilter = (categoryId: number) => {
+        if (filterCategoryId !== categoryId) {
+            setFilterCategoryId(categoryId);
+        } else if (filterCategoryId === categoryId) {
+            setFilterCategoryId(null);
         }
     };
 
@@ -271,7 +249,7 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
                                     <input
                                         id="filter"
                                         type="text"
-                                        value={value}
+                                        // value={value}
                                         onChange={onChangeFilter}
                                     />
                                 </>
@@ -279,9 +257,13 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
                             <div className="Home-categoryList">
                                 {filters.map((filter) => (
                                     <button
-                                        className="Home-categoryBtn"
+                                        className={`Home-categoryBtn ${
+                                            filter.id === filterCategoryId
+                                                ? "active"
+                                                : ""
+                                        }`}
                                         key={filter.id}
-                                        onClick={getFilter}
+                                        onClick={() => getFilter(filter.id)}
                                     >
                                         {filter.name}
                                     </button>
@@ -295,7 +277,7 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
                                 </div>
                             ) : choiceItems.length !== 0 ? (
                                 choiceItems.map((item) => {
-                                    if (filtering === "") {
+                                    if (!filterCategoryId) {
                                         return (
                                             <Content
                                                 key={item.id}
@@ -304,11 +286,11 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
                                         );
                                     } else {
                                         return (
-                                            item.category === filtering && (
+                                            item.categoryId ===
+                                                filterCategoryId && (
                                                 <Content
                                                     key={item.id}
                                                     item={item}
-                                                    userObj={userObj}
                                                 />
                                             )
                                         );
@@ -329,7 +311,8 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
                 <AddForm
                     onChange={onChange}
                     onSubmit={onSubmit}
-                    categoryValue={categoryValue}
+                    // categoryValue={categoryValue}
+                    categoryValue={""}
                     categoryRadio={categoryRadio}
                     categoryInput={categoryInput}
                     question={question}
@@ -344,10 +327,10 @@ const Home = ({ userObj }: { userObj: UserObj }) => {
                     filters={filters}
                 />
             )}
-            {floatingAlert && (
+            {isAlertOn && (
                 <Alert
-                    text={completeUpload ? "업로드 완료" : "업로드 중.."}
-                    idText={completeUpload ? "uploaded" : null}
+                    text={isUploadCompleted ? "업로드 완료" : "업로드 중.."}
+                    idText={isUploadCompleted ? "uploaded" : undefined}
                 />
             )}
         </div>
