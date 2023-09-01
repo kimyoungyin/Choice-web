19d8ef8b588eb52f0909cadd7c2c77af9c72c82f
kimyoungyin <yin199859@gmail.com>
2023-09-01 17:37:01 +0900

    issue #13 fix:  FullImageModal의 backgroundColor 제거 및 width, heigt를 이미지 비율에 맞게 고정

--- src/components/FullImageModal.tsx
+++ src/components/FullImageModal.tsx
@@ -1,3 +1,5 @@
+import { SyntheticEvent, useState } from "react";
+
 import { Image, Modal, ModalContent, ModalOverlay } from "@chakra-ui/react";
 
 interface FullImageModalProps {
@@ -7,6 +9,16 @@ interface FullImageModalProps {
 }
 
 const FullImageModal = ({ isOpen, onClose, src }: FullImageModalProps) => {
+    const [isWidthBiggerThanHeight, setIsWidthBiggerThanHeight] =
+        useState(true);
+
+    const handleImageState = (
+        event: SyntheticEvent<HTMLImageElement, Event>
+    ) => {
+        const { width, height } = event.target as HTMLImageElement;
+        setIsWidthBiggerThanHeight(width >= height);
+    };
+
     return (
         <Modal isOpen={isOpen} onClose={onClose} isCentered>
             <ModalOverlay />
@@ -14,8 +26,17 @@ const FullImageModal = ({ isOpen, onClose, src }: FullImageModalProps) => {
                 onClick={onClose}
                 display={"flex"}
                 alignItems={"center"}
+                backgroundColor={"transparent"}
+                boxShadow={"none"}
+                w={isWidthBiggerThanHeight ? "60%" : "auto"}
+                h={isWidthBiggerThanHeight ? "auto" : "60%"}
             >
-                <Image src={src} boxSize={"60%"} />
+                <Image
+                    src={src}
+                    w={isWidthBiggerThanHeight ? "full" : undefined}
+                    h={isWidthBiggerThanHeight ? undefined : "full"}
+                    onLoad={handleImageState}
+                />
             </ModalContent>
         </Modal>
     );
